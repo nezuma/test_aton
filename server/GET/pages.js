795ce0e17c0ed27ff1e527dcdb402add7c17e0b3
nezuma <110791833+nezuma@users.noreply.github.com>
2024-04-13 05:32:51 +0300

    init

--- server/GET/pages.js
+++ server/GET/pages.js
@@ -4,9 +4,11 @@ const temp = path.join(__dirname, '../../index.html');
 const pages = (req, res) => {
     switch(req.params.page) {
         case 'auth': 
+            if (req.session && req.session.User) return res.redirect('/main');
             res.sendFile(temp);
             break;
         case 'main':
+            if (req.session && !req.session.User) return res.redirect('/auth');
             res.sendFile(temp);
             break;
     }
